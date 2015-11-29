1b7e479ac1a457b77dcad0e150ed23c64a998921
dylanpavelko <dylan@dylanpavelko.com>
2015-11-29 12:33:38 -0800

    fixed bug with images

--- app/models/profile_image.rb
+++ app/models/profile_image.rb
@@ -1,7 +1,9 @@
 class ProfileImage < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image
+  has_attached_file :image,	
+  	:url => "/assets/profile_images/:id/:basename.:extension",
+	:path => ":rails_root/public/assets/profile_images/:id/:basename.:extension"
   validates_attachment :image, :size => { :in => 0..100.kilobytes }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
